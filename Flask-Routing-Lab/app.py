1eb357e3e616c86d86b8793cddbebd75e46d71ee
yoavsh24-meet <yoavisnotashuster@gmail.com>
2023-07-18 16:23:59 +0300

    final  HOORAY!

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -6,11 +6,17 @@ app = Flask(  # Create a flask app
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
 )
-
+#shop name, description, category section
 # Your code should be below
-
-
-
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/product")
+def product():
+    return render_template("product.html")
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
 
 # Your code should be above
 
